ea22fdfff01ea22f837ab0ead4d86811bf6d0498
devfrank9 <devfrank9@gmail.com>
2022-08-13 10:40:02 +0900

    first commit

---
+++ client/README.md
@@ -0,0 +1,15 @@
+# 프로젝트의 실행 방법
+
+## server
+
+1. cd server
+2. npm i
+3. npm start
+
+## client
+
+1. cd client
+2. npm i
+3. npm start
+
+# 데모 영상
---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default function Main() {
+  return <Container></Container>;
+}
+
+export const Container = styled.div`
+  width: 100%;
+  height: 10rem;
+`;
