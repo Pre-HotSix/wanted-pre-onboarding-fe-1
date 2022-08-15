0819dfa93342083e1442e609ea7cfbe210c43c27
devfrank9 <devfrank9@gmail.com>
2022-08-15 11:14:04 +0900

    Feat: "유효성 동적 디자인 추가"

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,20 +3,28 @@ import styled from 'styled-components';
 interface Prop {
   clickEvent?: () => void;
   buttonText?: string;
+  isActive?: boolean;
 }
 
 export default function Button(props: Prop) {
   return (
-    <Container>
-      <button onClick={props.clickEvent}>{props.buttonText}</button>
+    <Container isActive={props.isActive}>
+      <button onClick={props.clickEvent} disabled={props.isActive}>
+        {props.buttonText}
+      </button>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.div<{ isActive?: boolean }>`
   width: 100%;
   text-align: center;
   & > button {
     width: 50%;
+    background-color: ${prop => (prop.isActive ? 'gray' : 'red')};
+    cursor: ${prop => (prop.isActive ? '' : 'pointer')};
+    border: none;
+    border-radius: 3rem;
+    color: white;
   }
 `;
--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,23 +1,33 @@
+import React from 'react';
 import styled from 'styled-components';
 
 interface Prop {
   place?: string;
   type?: 'password' | 'email';
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  valid?: boolean;
 }
 
 export default function Input(props: Prop) {
   return (
-    <Container>
-      <input type={'text' || props.type} placeholder={props.place} />
+    <Container isValid={props.valid}>
+      <input
+        type={props.type}
+        placeholder={props.place}
+        value={props.value}
+        onChange={props.onChange}
+      />
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.div<{ isValid?: boolean }>`
   width: 100%;
   text-align: center;
   & > input {
     width: 80%;
     height: 3rem;
+    border: 1px solid ${prop => (prop.isValid ? 'red' : 'black')};
   }
 `;
