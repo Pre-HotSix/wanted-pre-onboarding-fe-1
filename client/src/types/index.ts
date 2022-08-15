ae38de528f7753664df6229725447f06699a7812
devfrank9 <devfrank9@gmail.com>
2022-08-15 11:13:41 +0900

    Feat: "자주 쓰이는 타입 선언"

---
+++ client/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface ISignBody {
+  email?: string;
+  password?: string;
+}
+export interface ITodo {
+  id?: number;
+  todo?: string;
+  isCompleted?: boolean;
+  userId?: number;
+}
+export interface ITodos {
+  todos: ITodo[];
+}
