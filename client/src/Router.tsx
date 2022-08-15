38c110e33852795cd8942ef59d3377fbeca672a6
devfrank9 <devfrank9@gmail.com>
2022-08-15 11:13:11 +0900

    Feat: " auth라우팅"

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,12 +1,35 @@
 import { Route, Routes } from 'react-router-dom';
 import { Main, Login, SignUp } from './pages';
+import PrivateRoute from './utils/PrivateRoute';
+import PublicRoute from './utils/PublicRoute';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/todo" element={<Main />} />
+      <Route
+        path="/"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <PublicRoute>
+            <SignUp />
+          </PublicRoute>
+        }
+      />
+      <Route
+        path="/todo"
+        element={
+          <PrivateRoute>
+            <Main />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
